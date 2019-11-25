72e24fc8656fab827cec81fc49dfc975288b5f62
Ivo Hutasoit <if09051@gmail.com>
2019-11-25 20:23:33 +0800

    Add chat model

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/golang/protobuf v1.3.2
 	google.golang.org/grpc v1.25.1
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
---
+++ model/domain/channel.go
@@ -0,0 +1,10 @@
+package domain
+
+import "gopkg.in/mgo.v2/bson"
+
+//Channel holds information about one specific channel in the room
+type Channel struct {
+	ID        bson.ObjectId `json:"id" bson:"_id"`
+	Name      string        `json:"name" bson:"name"`
+	CreatedBy string        `json:"created_by" bson:"created_by"`
+}
