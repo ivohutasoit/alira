644b7aecbbd4dc9c1e4c512b593852f25eebc2d6
Ivo Hutasoit <if09051@gmail.com>
2019-11-28 00:30:11 +0800

    Create encryption.go

---
+++ util/encryption.go
@@ -0,0 +1,49 @@
+package util
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+)
+
+func Encrypt(key []byte, plain []byte) (string, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	cipherText := make([]byte, aes.BlockSize+len(string(plain)))
+	iv := cipherText[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(cipherText[aes.BlockSize:], plain)
+
+	return base64.URLEncoding.EncodeToString(cipherText), nil
+}
+
+func Decrypt(key []byte, encrypted []byte) (string, error) {
+	cipherText, err := base64.URLEncoding.DecodeString(string(encrypted))
+	if err != nil {
+		return "", err
+	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	if len(cipherText) < aes.BlockSize {
+		return "", errors.New("Invalid cipher text")
+	}
+	iv := cipherText[:aes.BlockSize]
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(cipherText, cipherText)
+
+	return fmt.Sprintf("%s", ciphertext), nil
+}
