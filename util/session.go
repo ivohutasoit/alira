9514e5cb1c055fd126639d4de4a6846b24f805f7
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2019-12-03 17:44:56 +0800

    Change store using gin contrib

--- util/session.go
+++ util/session.go
@@ -1,18 +1,18 @@
 package util
 
 import (
-	"math"
 	"os"
 
-	"github.com/gorilla/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/markbates/goth/gothic"
 )
 
 // Store keep our sessions
-var Store *sessions.FilesystemStore
+var Store *cookie.Store
 
 func init() {
-	Store = sessions.NewFilesystemStore(os.TempDir(), []byte(os.Getenv("SECRET_KEY")))
-	Store.MaxLength(math.MaxInt64)
+	Store := cookie.NewStore([]byte(os.Getenv("SECRET_KEY")))
+	//Store = sessions.NewFilesystemStore(os.TempDir(), []byte(os.Getenv("SECRET_KEY")))
+	//Store.MaxLength(math.MaxInt64)
 	gothic.Store = Store
 }
