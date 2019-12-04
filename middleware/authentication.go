4a067c490d20a8e536d9cc0817f39b7ae4164a65
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2019-12-04 14:51:15 +0800

    Update authentication.go

--- middleware/authentication.go
+++ middleware/authentication.go
@@ -1,18 +1,31 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 
-	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"github.com/ivohutasoit/alira/common"
+	"github.com/ivohutasoit/alira/util"
 )
 
 func AuthenticationRequired(args ...interface{}) gin.HandlerFunc {
+	if 1 > len(args) {
+		panic("Redirect authentication url must be provide")
+	}
+
 	return func(c *gin.Context) {
 		session := sessions.Default(c)
 		token := session.Get("token")
 		if token == nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "user needs to be signed in to access this service"})
+			url := fmt.Sprintf("%s%s", c.Request.Host, c.Request.URL.Path)
+			url, err := util.Encrypt(url, common.SecretKey)
+			if err != nil {
+				fmt.Println(err)
+			}
+			redirect := fmt.Sprintf("%s?%s", args[0].(string), url)
+			c.Redirect(http.StatusMovedPermanently, redirect)
 			c.Abort()
 			return
 		}
