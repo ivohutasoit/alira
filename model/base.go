11d7000d79490e3db8fa468d6108620120cce350
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2019-12-10 15:08:43 +0800

    Add Base Model

---
+++ model/base.go
@@ -0,0 +1,58 @@
+package model
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/ivohutasoit/alira/model/domain"
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+)
+
+var db *gorm.DB
+
+type BaseModel struct {
+	ID        string     `gorm:"primary_key" json:"id" bson:"id"`
+	CreatedAt time.Time  `json:"-"`
+	UpdatedAt time.Time  `json:"-"`
+	DeletedAt *time.Time `sql:"index" json:"-"`
+}
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("error loading .env file")
+	}
+
+	engine := os.Getenv("DATABASE.ENGINE")
+	var conn *gorm.DB
+	if engine == "sqlite3" {
+		file := os.Getenv("DATABASE.file")
+		conn, err = gorm.Open(engine, file)
+	} else if engine == "postgres" {
+		host := os.Getenv("DATABASE.HOST")
+		port := os.Getenv("DATABASE.PORT")
+		schema := os.Getenv("DATABASE.SCHEMA")
+		username := os.Getenv("DATABASE.USERNAME")
+		password := os.Getenv("DATABASE.PASSWORD")
+
+		uri := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s",
+			host, port, username, schema, password)
+
+		fmt.Println(uri)
+		conn, err = gorm.Open(engine, uri)
+	}
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	db = conn
+	db.Debug().AutoMigrate(&domain.user{})
+	db.Debug().AutoMigrate(&domain.UserProfile{})
+}
+
+func GetDatabase() *gorm.DB {
+	return db
+}
