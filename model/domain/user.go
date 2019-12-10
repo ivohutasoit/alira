03c9cf3fc08a446e333822349d9aa74d378fc2ca
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2019-12-10 15:09:01 +0800

    Change User Domain Model

--- model/domain/user.go
+++ model/domain/user.go
@@ -2,33 +2,46 @@ package domain
 
 import (
 	"encoding/gob"
-	"github.com/dgrijalva/jwt-go"
 	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/ivohutasoit/alira/model"
 )
 
 func init() {
 	gob.Register(&User{})
-}
-
-// UserKey is key to save user to context
-type UserKey int
-
-// User hold information about an user
-type User struct {
-	ID          string    `json:"id" bson:"_id"`
-	Email       string    `json:"email" bson:"email"`
-	Name        string    `json:"name" bson:"name"`
-	FirstName   string    `json:"first_name" bson:"first_name"`
-	LastName    string    `json:"last_name" bson:"last_name"`
-	NickName    string    `json:"nick_name" bson:"nick_name"`
-	Description string    `json:"description" bson:"description"`
-	UserID      string    `json:"user_id" bson:"user_id"`
-	AvatarURL   string    `json:"avatar_url" bson:"avatar_url"`
-	CreatedAt   time.Time `json:"created_at" bson:"created_at"`
-	Active      bool      `json:"active" bson:"active"`
+	gob.Register(&UserProfile{})
 }
 
 type UserToken struct {
 	jwt.StandardClaims
 	UserID string `json:"user_id"`
+}
+
+// User hold information about an user
+type User struct {
+	model.BaseModel
+	Username 	string 	`json:"username" bson:"username"`
+	Email 		string 	`json:"email" bson:"email"`
+	Mobile 		string 	`json:"mobile" bson:"mobile"`
+	Avatar 		string 	`json:"avatar" bson:"avatar"`
+	Active 		bool 	`json:"active" bson:"active"`
+}
+
+func (User) TableName() string {
+	return "users"
+}
+
+
+type UserProfile struct {
+	model.BaseModel
+	Name        string `json:"name" bson:"name"`
+	FirstName   string `json:"first_name" bson:"first_name"`
+	LastName    string `json:"last_name" bson:"last_name"`
+	NickName    string `json:"nick_name" bson:"nick_name"`
+	Description string `json:"description" bson:"description"`
+}
+
+func (UserProfile) TableName() string {
+	return "profiles"
 }
