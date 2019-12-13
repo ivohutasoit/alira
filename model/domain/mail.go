507b74448973fd7696c9dff6f0827acfa147b85b
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2019-12-13 12:31:03 +0800

    Change mail type fields

--- model/domain/mail.go
+++ model/domain/mail.go
@@ -1,20 +1,23 @@
 package domain
 
 type Mail struct {
-	From    string
-	To      []string
-	Subject string
-	Body    string
+	From     string
+	To       []string
+	Subject  string
+	Template string
+	Data     map[interface{}]interface{}
 }
 
 const (
 	MIME = "MIME-VERSION: 1.0;\nContent-Type: text/html; charset:\"UTF-8\";\n\n"
 )
 
-func (m *Mail) NewMailer(to []string, subject string, body string) *Mail {
+func (m *Mail) NewMail(from string, to []string, subject string, template string, data map[interface{}]interface{}) *Mail {
 	return &Mail{
-		To:      to,
-		Subject: subject,
-		Body:    body,
+		From:     from,
+		To:       to,
+		Subject:  subject,
+		Template: template,
+		Data:     data,
 	}
 }
