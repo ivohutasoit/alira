3859f00a2ea165834e49a60330998060a0712cad
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2020-01-03 20:57:23 +0700

    Change constant name

--- constant/general.go
+++ constant/general.go
@@ -3,6 +3,6 @@ package constant
 const (
 	Letters           = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	Numbers           = "0123456789"
-	MeesageBufferSize = 256
+	MessageBufferSize = 256
 	SocketBufferSize  = 1024
 )
