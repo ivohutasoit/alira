393c43ec7e0e806711a76a591e340fbe92df9c81
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2020-01-09 17:37:12 +0800

    Support multiple email address

--- service/mail-service.go
+++ service/mail-service.go
@@ -12,20 +12,24 @@ import (
 type MailService struct{}
 
 func (ms *MailService) Send(mail *domain.Mail) (map[interface{}]interface{}, error) {
-	body, _ := util.ParseMailTemplate(mail.Template, mail.Data)
-	fullbody := fmt.Sprintf("From: %s\r\nTo: %s\r\nSubject: %s\r\n%s\r\n%s", mail.From,
-		mail.To[0],
-		mail.Subject,
-		domain.MIME,
-		body,
-	)
-	mailSMTP := fmt.Sprintf("%s:%s", os.Getenv("SMTP.HOST"), os.Getenv("SMTP.PORT"))
-	if err := smtp.SendMail(mailSMTP, smtp.PlainAuth("", mail.From,
-		os.Getenv("SMTP.PASSWORD"), os.Getenv("SMTP.HOST")), os.Getenv("SMTP.NAME"),
-		mail.To, []byte(fullbody)); err != nil {
-		return nil, err
+	var message string
+	for _, to := range mail.To {
+		body, _ := util.ParseMailTemplate(mail.Template, mail.Data)
+		fullbody := fmt.Sprintf("From: %s\r\nTo: %s\r\nSubject: %s\r\n%s\r\n%s", mail.From,
+			to,
+			mail.Subject,
+			domain.MIME,
+			body,
+		)
+		mailSMTP := fmt.Sprintf("%s:%s", os.Getenv("SMTP.HOST"), os.Getenv("SMTP.PORT"))
+		if err := smtp.SendMail(mailSMTP, smtp.PlainAuth("", mail.From,
+			os.Getenv("SMTP.PASSWORD"), os.Getenv("SMTP.HOST")), os.Getenv("SMTP.NAME"),
+			mail.To, []byte(fullbody)); err != nil {
+			return nil, err
+		}
+		message += fmt.Sprintf("Mail has been sent to %s \n", to)
 	}
 	return map[interface{}]interface{}{
-		"message": fmt.Sprintf("Mail has been sent to %s", mail.To[0]),
+		"message": message,
 	}, nil
 }
