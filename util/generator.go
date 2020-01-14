040b47e9852959a238cc97db3c6fdceefe88fc7b
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2020-01-14 18:24:55 +0800

    Update generator for National Code

--- util/generator.go
+++ util/generator.go
@@ -1,10 +1,14 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
+	"strconv"
 	"time"
 
 	"github.com/ivohutasoit/alira/constant"
+	"github.com/ivohutasoit/alira/model"
+	"github.com/ivohutasoit/alira/model/domain"
 )
 
 func GenerateToken(length int) string {
@@ -31,6 +35,39 @@ func GenerateQrcode(length int) string {
 	return string(b)
 }
 
-func GenerateNationID(args ...interface{}) string {
-	return "NATIONCODE"
+func GenerateNationalCode(args ...interface{}) string {
+	//Example Nomor NIK : 1234567890ABCDEF
+	//12 nomor merupakan kode provinsi
+	//34 nomor merupakan kode kotamadya atau kabupaten kota
+	//56 nomor kode kecamatan
+	//78 nomor tanggal lahir
+	//90 nomor bulan lahir
+	//AB nomor tahun lahir
+	//CDEF nomor registrasi kependudukan
+
+	// Country code 2 characters ID=62
+	// BirthDate 8 characters format yyyyMMDD
+	// Sequence 4 characters
+	date, _ := args[1].(time.Time)
+
+	code := fmt.Sprintf("%d%d%02d%02d", 62, date.Year(), date.Month(), date.Day())
+
+	var identities []domain.Identity
+	model.GetDatabase().Where("code LIKE ?", code+"%").Find(&identities).Order("code DESC")
+
+	var identity domain.Identity
+	if identities != nil {
+		identity = identities[0]
+	}
+
+	var seq int
+	if identity.BaseModel.ID != "" {
+		if n, err := strconv.Atoi(identity.Code[10:len(identity.Code)]); err == nil {
+			seq = n + 1
+		} else {
+			fmt.Println(seq, "is not an integer.")
+		}
+	}
+
+	return fmt.Sprintf("%s%06d", code, seq)
 }
