3fc370daba590506c80f3204d529dbed9e5252f5
Softh Axi <softhaxi@gmail.com>
2020-01-16 09:02:08 +0800

    Change userid to subscriber id to support community

--- model/domain/package.go
+++ model/domain/package.go
@@ -10,19 +10,20 @@ import (
 
 type Subscribe struct {
 	model.BaseModel
-	Code      string    `json:"code" bson:"code"`
-	UserID    string    `json:"user_id" bson:"user_id"`
-	User      User      `json:"-" gorm:"foreignkey:UserID"`
-	Purpose   string    `json:"purpose" bson:"purpose"`
-	Signature string    `json:"signature" bson:"signature"`
-	NotBefore time.Time `json:"not_before" bson:"not_before"`
-	NotAfter  time.Time `json:"not_after" bson:"not_after"`
-	AgreedAt  time.Time `json:"agreed_at" bson:"agreed_at"`
+	Code         string    `json:"code" bson:"code"`
+	Class        string    `json:"-" bson:"-" gorm:"default:USERSUBSCRIPTION"`
+	SubscriberID string    `json:"subscriber_id" bson:"subscriber_id" sql:"index"`
+	Purpose      string    `json:"purpose" bson:"purpose"`
+	Signature    string    `json:"signature" bson:"signature"`
+	NotBefore    time.Time `json:"not_before" bson:"not_before"`
+	NotAfter     time.Time `json:"not_after" bson:"not_after"`
+	AgreedAt     time.Time `json:"agreed_at" bson:"agreed_at"`
 }
 
 func (subscribe *Subscribe) BeforeCreate(scope *gorm.Scope) error {
 	scope.SetColumn("ID", uuid.New().String())
 	scope.SetColumn("NotBefore", time.Now())
+	scope.SetColumn("AgreedAt", time.Now())
 	return nil
 }
 
