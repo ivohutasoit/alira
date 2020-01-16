44aa24078729934b6750c188d6281f8e29da5e73
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2020-01-16 17:36:22 +0800

    Add some services and classes

--- model/domain/log.go
+++ model/domain/log.go
@@ -10,7 +10,7 @@ import (
 
 type Log struct {
 	model.BaseModel
-	Class      string    `json:"-" bson:"-" gorm:"default:FINANCIAL"`
+	Class      string    `json:"-" bson:"-" gorm:"default:FINANCIAL"` // FINANCIAL, TRANSACTION
 	ActionDate time.Time `json:"action_date" bson:"action_date"`
 	ActionBy   string    `json:"action_by" bson:"action_by" sql:"index"`
 	Status     string    `json:"status" bson:"status"`
@@ -31,7 +31,7 @@ type FinancialLog struct {
 	ID             string  `json:"id" bson:"id" gorm:"primary_key"`
 	Log            Log     `json:"-" gorm:"foreignkey:ID"`
 	Type           string  `json:"type" bson:"type" gorm:"default:DEBIT"` // DEBIT: Money Out from wallet; CREDIT: Money In to wallet
-	ServiceID      string  `json:"service_id" bson:"service_id"`          // TOPUP, PURCHASE
+	ServiceID      string  `json:"service_id" bson:"service_id"`          // TOPUP, PURCHASE, PAYMENT
 	Amount         float64 `json:"amount" bson:"amount"`
 	Fee            float64 `json:"fee" bson:"fee"`
 	ReferenceNo    string  `json:"reference_no" bson:"reference_no"`
