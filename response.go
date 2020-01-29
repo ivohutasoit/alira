6067422f638ecb0a442bff8af86b6f7ba5d7c430
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2020-01-29 17:00:36 +0800

    Repackage types

---
+++ response.go
@@ -0,0 +1,11 @@
+package main
+
+import "encoding/json"
+
+type Response struct {
+	Code    int             `json:"code" bson:"code"`
+	Status  string          `json:"status" bson:"stats"`
+	Message string          `json:"message" bson:"message`
+	Error   string          `json:"error" bson:"error"`
+	Data    json.RawMessage `json:"data" bson:"data"`
+}
