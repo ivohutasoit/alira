196676f394a8340b145b6966e2ed005918a11ff6
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2020-01-29 17:01:09 +0800

    Add interface middleware

---
+++ middleware.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+type AuthenticationMiddleware interface {
+	SessionRequired(args ...interface{}) gin.HandlerFunc
+	TokenRequired(args ...interface{}) gin.HandlerFunc
+}
+
+type AccessMiddleware interface {
+	AppAdminRequired(args ...interface{}) gin.HandlerFunc
+	CustomerAdminRequired(args ...interface{}) gin.HandlerFunc
+}
